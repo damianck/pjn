90aa85d68ba2b61ac4817105cbd86d1af97c8809
Daman Wtorek <damian.wtr@gmail.com>
2017-06-11 20:25:51 +0200

    changed strimmer

--- src/main/java/com/pjn/pl/Strimmer.java
+++ src/main/java/com/pjn/pl/Strimmer.java
@@ -10,13 +10,13 @@ public class Strimmer {
 	public static void main(String[] args) throws IOException {
 
 		PolishStemmer stemmer = new PolishStemmer();
-
 		String in = "Nie zabrak³o oczywiœcie wpadek. Najwiêkszym zaskoczeniem okaza³ siê dla nas strój Katarzyny Zieliñskiej, której ewidentnie o coœ chodzi³o, ale wci¹¿ nie wiemy o co.";
 		for (String t : in.toLowerCase(new Locale("pl")).split("[\\s\\.\\,]+")) {
 			System.out.println("> '" + t + "'");
 
 			for (WordData wd : stemmer.lookup(t)) {
-				System.out.print(" - " + (wd.getStem() == null ? "<null>" : wd.getStem()) + ", " + wd.getTag());
+				System.out.print(" - " + (wd.getStem() == null ? "<null>" : wd.getStem()) + ", " + 
+				        wd.getTag());
 			}
 			System.out.println();
 		}
