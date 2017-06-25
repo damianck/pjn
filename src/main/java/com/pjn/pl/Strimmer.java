ca8681415af448bcca573baac6fdbe88d8c99daa
damianck <damian.wtr@gmail.com>
2017-06-25 21:12:01 +0200

    refactored code and added event dispatcher

--- src/main/java/com/pjn/pl/Strimmer.java
+++ src/main/java/com/pjn/pl/Strimmer.java
@@ -1,28 +1,15 @@
 package com.pjn.pl;
 
-import java.io.IOException;
-import java.util.Locale;
-
-import morfologik.stemming.WordData;
+import javax.swing.*;
+import java.awt.*;
 
 public class Strimmer {
 	public static void main(String[] args) throws Exception {
-		new WindowInterface(1);
-
+		EventQueue.invokeLater(() -> {
+			JFrame frame = new Window();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setResizable(false);
+			frame.setVisible(true);
+		});
 	}
-
-//	public static void main(String[] args) throws IOException {
-//
-//		PolishStemmer stemmer = new PolishStemmer();
-//		String in = "Nie zabrak?o oczywi?cie wpadek. Najwi?kszym zaskoczeniem okaza? si? dla nas str?j Katarzyny Zieli?skiej, kt?rej ewidentnie o co? chodzi?o, ale wci?? nie wiemy o co";
-//		for (String t : in.toLowerCase(new Locale("pl")).split("[\\s\\.\\,]+")) {
-//			System.out.println("> '" + t + "'");
-//
-//			for (WordData wd : stemmer.lookup(t)) {
-//				System.out.print(" - " + (wd.getStem() == null ? "<null>" : wd.getStem()));
-//			}
-//			System.out.println();
-//		}
-//	}
-
 }
